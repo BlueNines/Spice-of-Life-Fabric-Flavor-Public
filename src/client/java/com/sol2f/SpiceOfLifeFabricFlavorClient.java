030ab9ac616d8798b017a7d83d14da9a5719f994
xianfish <zl3068@outlook.com>
2025-09-20 18:56:14 +0800

    chore: initial commit

---
+++ src/client/java/com/sol2f/SpiceOfLifeFabricFlavorClient.java
@@ -0,0 +1,10 @@
+package com.sol2f;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SpiceOfLifeFabricFlavorClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
